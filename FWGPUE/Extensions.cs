293c886e6ee800e9a7b3b5753cede0fac1c66ba9
Gavin White <horace5413@gmail.com>
2023-01-15 22:37:36 -0800

    move extensions to own file

---
+++ FWGPUE/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+namespace FWGPUE;
+
+public static class Extensions {
+    public static void ChangeBy(ref this Vector3 me, Vector3 change) {
+        me += change;
+    }
+}
