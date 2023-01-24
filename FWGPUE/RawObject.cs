46393b351948a7ebee867f247da90a80c36ab9d2
Gavin White <horace5413@gmail.com>
2023-01-23 21:54:43 -0800

    update RawObject for changed scale

--- FWGPUE/RawObject.cs
+++ FWGPUE/RawObject.cs
@@ -1,8 +1,6 @@
 ﻿using Silk.NET.OpenGL;
 using FWGPUE.IO;
 using System.Numerics;
-using System.Runtime.CompilerServices;
-using Silk.NET.Vulkan;
 
 namespace FWGPUE;
 
@@ -15,19 +13,19 @@ class RawObject : IDisposable {
     public Texture Texture { get; set; }
     public Shader Shader { get; set; }
 
-    readonly float[] Vertices = {
+    public float[] Vertices = {
         //x     y      z     u   v
          0.5f,  0.5f, 0.0f, 1f, 0f,
          0.5f, -0.5f, 0.0f, 1f, 1f,
         -0.5f, -0.5f, 0.0f, 0f, 1f,
         -0.5f,  0.5f, 0.0f, 0f, 0f
     };
-    readonly uint[] Indices = {
+    public uint[] Indices = {
         0, 1, 3,
         1, 2, 3
     };
 
-    public void Draw(GL Gl, Engine context) {
+    public virtual void Draw(GL Gl, Engine context) {
         VAO.Bind();
         Shader.Use();
         Texture.Bind();
@@ -38,7 +36,7 @@ class RawObject : IDisposable {
             Matrix4x4.CreateRotationZ(Engine.TurnsToRadians(Transform.Rotation.Z)) *
             Matrix4x4.CreateRotationY(Engine.TurnsToRadians(Transform.Rotation.Y)) *
             Matrix4x4.CreateRotationX(Engine.TurnsToRadians(Transform.Rotation.X)) *
-            Matrix4x4.CreateScale(Transform.Scale) *
+            Matrix4x4.CreateScale(Transform.Scale.X, Transform.Scale.Y, 1) *
             Matrix4x4.CreateTranslation(Transform.Position);
         var view = Matrix4x4.CreateLookAt(context.CameraPos, context.CameraTarget, context.CameraUp);
         var projection = Matrix4x4.CreatePerspectiveFieldOfView(Engine.DegreesToRadians(45.0f), (float)context.Config.ScreenWidth / context.Config.ScreenHeight, 0.1f, 100.0f);
