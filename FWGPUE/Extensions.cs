75239b6967c2cd6ccdc5a1e29e54911a93f321df
Gavin White <horace5413@gmail.com>
2023-01-29 13:09:06 -0800

    .squared() extensions for float and int

--- FWGPUE/Extensions.cs
+++ FWGPUE/Extensions.cs
@@ -6,4 +6,15 @@ public static class Extensions {
     public static void ChangeBy(ref this Vector3 me, Vector3 change) {
         me += change;
     }
+
+    #region math helper extensions
+
+    public static int Squared(this int me) {
+        return me * me;
+    }
+    public static float Squared(this float me) {
+        return me * me;
+    }
+
+    #endregion math helper extensions
 }
