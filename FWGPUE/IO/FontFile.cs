1f7f05302970d2c3d9febe3faf936c3517eabc80
Gavin White <horace5413@gmail.com>
2023-01-28 17:25:54 -0800

    create fontfile

---
+++ FWGPUE/IO/FontFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWGPUE.IO;
+class FontFile : DataMarkupFile {
+    public string DefaultFont { get; protected set; }
+    public ByteFile Default => FontData[DefaultFont];
+
+    public Dictionary<string, ByteFile> FontData { get; } = new();
+
+    public override void Load() {
+        // load and parse data using base class
+        base.Load();
+
+        if (TryGetToken("Default", out var defaultFont) && defaultFont is not null) {
+            DefaultFont = defaultFont.Contents.Value;
+            Log.Info($"default font: {DefaultFont}");
+        }
+        else {
+            Log.Warn($"no default font in {Location!.Name}");
+        }
+
+        if (TryGetToken("Fonts", out var fonts) && fonts is not null) {
+            string[] fontCollection = fonts.Contents.Collection;
+            for (int i = 0; i < fontCollection.Length; i += 2) {
+                string fontName = fontCollection[i];
+                string fontPath = fontCollection[i + 1];
+
+                ByteFile fontFile = new ByteFile(new(Location!.Path, fontPath));
+                if (fontFile.Location!.Exists()) {
+                    fontFile.Load();
+                    FontData[fontName] = fontFile;
+                }
+                else {
+                    Log.Error($"font {fontName} missing file at {fontPath} in {Location!.Name}");
+                }
+            }
+        }
+        else {
+            Log.Error($"no fonts defined in {Location!.Name}");
+        }
+    }
+
+    public FontFile(EngineFileLocation header) : base(header) { }
+}
+
