80a47f02f9ef01d74f2626ab8a937fd282418b1f
Gavin White <horace5413@gmail.com>
2023-02-06 12:27:05 -0800

    dead simple main menu

--- FWGPUE/Scenes/MainMenu.cs
+++ FWGPUE/Scenes/MainMenu.cs
@@ -2,6 +2,9 @@
 
 namespace FWGPUE.Scenes;
 
+using FWGPUE.IO;
+using static ImGuiNET.ImGuiWindowFlags;
+
 class MainMenu : Scene {
     public override void Load(Engine context) { }
     public override void Unload(Engine context) { }
@@ -11,6 +14,18 @@ class MainMenu : Scene {
     }
 
     public override void Render(Engine context) {
-        IM.Button("testing");
+        IM.SetNextWindowSize(new(Config.Instance.ScreenWidth, Config.Instance.ScreenHeight));
+        IM.Begin("mainMenuContainer", NoTitleBar | NoBackground | NoResize);
+
+        IM.SetCursorPos(new(Config.Instance.ScreenWidth / 2 - 100, Config.Instance.ScreenHeight / 4));
+        IM.Button("play", new(100, 20));
+
+        IM.SetCursorPos(new(Config.Instance.ScreenWidth / 2 - 100, Config.Instance.ScreenHeight / 3));
+        if (IM.Button("quit", new(100, 20))) {
+            // close
+            context.ChangeToScene(null);
+        }
+
+        IM.End();
     }
 }
