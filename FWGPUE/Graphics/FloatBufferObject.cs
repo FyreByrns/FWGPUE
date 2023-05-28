55619ff43e582323e5bdbb64f60508e71339374a
Gavin White <horace5413@gmail.com>
2023-05-28 12:05:09 -0700

    create FloatBufferObject to simplify code

---
+++ FWGPUE/Graphics/FloatBufferObject.cs
@@ -0,0 +1,51 @@
+﻿using Silk.NET.OpenGL;
+
+namespace FWGPUE.Graphics;
+
+class FloatBufferObject {
+    public uint Handle;
+
+    public void AttributePointer(uint vertexArray, int slot, int slots, int size, int vertexSize) {
+        Gl.BindVertexArray(vertexArray);
+        Bind();
+        unsafe {
+            for (int i = 0; i < slots; i++) {
+                Log.Info($"{i + 1} of {slots} from {slot} ({slot + i} overall)");
+                Gl.EnableVertexArrayAttrib(vertexArray, (uint)(slot + i));
+                Gl.VertexAttribPointer((uint)(slot + i), size, VertexAttribPointerType.Float, false, (uint)(vertexSize * sizeof(float)), (void*)(vertexSize * sizeof(float) * i));
+            }
+        }
+    }
+    public void Instanced(uint vertexArray, int slot, int slots, uint divisor) {
+        Gl.BindVertexArray(vertexArray);
+        Bind();
+        unsafe {
+            for (int i = 0; i < slots; i++) {
+                Gl.VertexAttribDivisor((uint)(slot + i), divisor);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Set data in the buffer object by some starting index.
+    /// <para>Index is by <typeparamref name="T"/>, not index * sizeof <typeparamref name="T"/>.</para>
+    /// <para>So the second element of a float array would be 1, not 4.</para>
+    /// </summary>
+    public unsafe void SetData<T>(uint vertexArray, params T[] data)
+        where T : unmanaged {
+
+        Bind();
+        // actually set the data
+        fixed (void* d = data) {
+            Gl.BufferData(BufferTargetARB.ArrayBuffer, (uint)(data.Length * sizeof(T)), d, BufferUsageARB.StaticDraw);
+        }
+    }
+
+    public void Bind() {
+        Gl.BindBuffer(BufferTargetARB.ArrayBuffer, Handle);
+    }
+
+    public FloatBufferObject() {
+        Handle = Gl.GenBuffer();
+    }
+}
--- FWGPUE/Graphics/SpriteRenderStage.cs
+++ FWGPUE/Graphics/SpriteRenderStage.cs
@@ -1,23 +1,34 @@
 ﻿using Silk.NET.OpenGL;
-using System.Collections.Immutable;
 using System.Numerics;
 
 namespace FWGPUE.Graphics;
 
 class SpriteRenderStage : RenderStage {
     static float[] quadVertices = {
-        -0.5f,  0.5f, 0.0f, 0f, 1f, // B
-        -0.5f, -0.5f, 0.0f, 0f, 0f, // A
-         0.5f, -0.5f, 0.0f, 1f, 0f, // D
-                                   
-        -0.5f,  0.5f, 0.0f, 0f, 1f, // B
-         0.5f, -0.5f, 0.0f, 1f, 0f, // D
-         0.5f,  0.5f, 0.0f, 1f, 1f, // C
+        -0.5f,  0.5f, 0.0f, // B
+        -0.5f, -0.5f, 0.0f, // A
+         0.5f, -0.5f, 0.0f, // D
+                           
+        -0.5f,  0.5f, 0.0f, // B
+         0.5f, -0.5f, 0.0f, // D
+         0.5f,  0.5f, 0.0f, // C
     };
+    static float[] quadUVs = {
+        0f, 1f, // B
+        0f, 0f, // A
+        1f, 0f, // D
+               
+        0f, 1f, // B
+        1f, 0f, // D
+        1f, 1f, // C
+    };
+
     uint quadVAO;
-    uint quadVBO;
-    uint offsetVBO;
-    uint uvVBO;
+
+    FloatBufferObject bo_QuadVertices;
+    FloatBufferObject bo_QuadUVs;
+    FloatBufferObject i_bo_Offsets;
+    FloatBufferObject i_bo_AtlasUVs;
 
     Shader AtlasedSpriteShader;
 
@@ -55,23 +66,9 @@ class SpriteRenderStage : RenderStage {
             uvs[i] = new Vector4(rect.X, rect.Y, rect.Width, rect.Height);
         });
 
-
         Gl.BindVertexArray(quadVAO);
-        Gl.BindBuffer(BufferTargetARB.ArrayBuffer, offsetVBO);
-        // push sprite positions to gpu
-        unsafe {
-            fixed (Matrix4x4* data = offsets) {
-                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(sizeof(Matrix4x4) * offsets.Length), data, BufferUsageARB.StaticDraw);
-            }
-        }
-
-        // push UVs to gpu
-        Gl.BindBuffer(BufferTargetARB.ArrayBuffer, uvVBO);
-        unsafe {
-            fixed (Vector4* data = uvs) {
-                Gl!.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(sizeof(Vector4) * uvs.Length), data, BufferUsageARB.StaticDraw);
-            }
-        }
+        i_bo_Offsets.SetData(quadVAO, offsets);
+        i_bo_AtlasUVs.SetData(quadVAO, uvs);
 
         // draw sprites instanced
         AtlasedSpriteShader.Use();
@@ -87,53 +84,25 @@ class SpriteRenderStage : RenderStage {
         AtlasedSpriteShader = new Shader(new("assets/sprite.shader"));
 
         quadVAO = Gl.GenVertexArray();
-        quadVBO = Gl.GenBuffer();
-        offsetVBO = Gl.GenBuffer();
-        uvVBO = Gl.GenBuffer();
 
-        Gl.BindBuffer(GLEnum.ArrayBuffer, quadVBO);
-        unsafe {
-            fixed (float* data = quadVertices) {
-                Gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint)(quadVertices.Length * sizeof(float)), data, BufferUsageARB.StaticDraw);
-            }
-        }
+        int slot = 0;
+        bo_QuadVertices = new();
+        bo_QuadVertices.AttributePointer(quadVAO, slot, 1, 3, 3);
+        bo_QuadVertices.SetData(quadVAO, quadVertices);
+        slot++;
 
-        // setup shader input data
-        uint slot = 0;
+        bo_QuadUVs = new();
+        bo_QuadUVs.AttributePointer(quadVAO, slot, 1, 2, 2);
+        bo_QuadUVs.SetData(quadVAO, quadUVs);
+        slot++;
 
-        Gl.BindVertexArray(quadVAO);
-        Gl.EnableVertexAttribArray(slot);
-        Gl.EnableVertexAttribArray(slot + 1);
-        unsafe {
-            Gl!.VertexAttribPointer(slot + 0, 3, VertexAttribPointerType.Float, false, sizeof(float) * 5, (void*)0);
-            Gl!.VertexAttribPointer(slot + 1, 2, VertexAttribPointerType.Float, false, sizeof(float) * 5, (void*)(sizeof(float) * 3));
-        }
-        slot += 2;
-
-        // setup offset buffer
-        Gl.BindBuffer(BufferTargetARB.ArrayBuffer, offsetVBO);
-        Gl.EnableVertexAttribArray(slot + 0); // mat4 takes up 4 slots as float4s
-        Gl.EnableVertexAttribArray(slot + 1); // ..
-        Gl.EnableVertexAttribArray(slot + 2); // ..
-        Gl.EnableVertexAttribArray(slot + 3); // ..
-        unsafe {
-            Gl.VertexAttribPointer(slot + 0, 4, VertexAttribPointerType.Float, false, (uint)sizeof(Matrix4x4), (void*)(sizeof(float) * 4 * 0));
-            Gl.VertexAttribPointer(slot + 1, 4, VertexAttribPointerType.Float, false, (uint)sizeof(Matrix4x4), (void*)(sizeof(float) * 4 * 1));
-            Gl.VertexAttribPointer(slot + 2, 4, VertexAttribPointerType.Float, false, (uint)sizeof(Matrix4x4), (void*)(sizeof(float) * 4 * 2));
-            Gl.VertexAttribPointer(slot + 3, 4, VertexAttribPointerType.Float, false, (uint)sizeof(Matrix4x4), (void*)(sizeof(float) * 4 * 3));
-        }
-        Gl.VertexAttribDivisor(slot + 0, 1); // instanced row 1
-        Gl.VertexAttribDivisor(slot + 1, 1); // ..         .. 2
-        Gl.VertexAttribDivisor(slot + 2, 1); // ..         .. 3
-        Gl.VertexAttribDivisor(slot + 3, 1); // ..         .. 4
+        i_bo_Offsets = new();
+        i_bo_Offsets.AttributePointer(quadVAO, slot, 4, 4, 4);
+        i_bo_Offsets.Instanced(quadVAO, slot, 4, 1);
         slot += 4;
 
-        // setup uv buffer
-        Gl!.BindBuffer(BufferTargetARB.ArrayBuffer, uvVBO);
-        Gl!.EnableVertexAttribArray(slot);
-        unsafe {
-            Gl!.VertexAttribPointer(slot, 4, VertexAttribPointerType.Float, false, sizeof(float) * 4, (void*)0);
-        }
-        Gl!.VertexAttribDivisor(slot, 1); // instanced
+        i_bo_AtlasUVs = new();
+        i_bo_AtlasUVs.AttributePointer(quadVAO, slot, 1, 4, 2);
+        i_bo_AtlasUVs.Instanced(quadVAO, slot, 1, 1);
     }
 }
